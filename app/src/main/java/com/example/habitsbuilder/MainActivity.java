f638797354700de6436a8186cc8998c95021a3fc
quanphanhong <18520239@gm.uit.edu.vn>
2020-09-15 05:36:46 +0700

    layout design

--- app/src/main/java/com/example/habitsbuilder/MainActivity.java
+++ app/src/main/java/com/example/habitsbuilder/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.habitsbuilder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout habit_list;
+    BottomNavigationView navigationView;
+    NavHostFragment fragment;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        menuItemsChanged();
+    }
+
+    private void init() {
+        habit_list = (LinearLayout) findViewById(R.id.habit_list);
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation_bar);
+
+
+        fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        //navController = fragment.getNavController();
+        navController = Navigation.findNavController(this, R.id.fragment);
+    }
+
+    private void menuItemsChanged() {
+        navigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.DailyTask:
+                                navController.navigate(R.id.dailyTaskFragment);
+                                break;
+                            case R.id.HabitList:
+                                navController.navigate(R.id.habitListFragment);
+                                break;
+                            case R.id.Pet:
+                                navController.navigate(R.id.petsFragment);
+                                break;
+                        }
+                        return true;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+        return true;
     }
 }
