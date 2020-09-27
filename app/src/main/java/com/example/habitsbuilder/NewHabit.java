5020b2e83e752806e991df747651c9f44ea22f75
quanphanhong <18520239@gm.uit.edu.vn>
2020-09-27 09:17:32 +0700

    resolve some 'new habit' UI problems

--- app/src/main/java/com/example/habitsbuilder/NewHabit.java
+++ app/src/main/java/com/example/habitsbuilder/NewHabit.java
@@ -7,12 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Switch;
 
 public class NewHabit extends AppCompatActivity {
 
     Spinner sp_frequency;
+
+    LinearLayout panel_alert;
+    Switch sw_alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,27 @@ public class NewHabit extends AppCompatActivity {
         setContentView(R.layout.activity_new_habit);
 
         init();
-
         prepFrequency();
-
     }
 
     private void init() {
         sp_frequency = (Spinner) findViewById(R.id.habit_frequency_spinner);
+
+        panel_alert = (LinearLayout) findViewById(R.id.habit_reminding_time_setting);
+        sw_alert = (Switch) findViewById(R.id.habit_alert_switch);
+        panel_alert.setVisibility(View.INVISIBLE);
+        sw_alertChangedListener();
+
+    }
+
+    private void sw_alertChangedListener() {
+        sw_alert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) panel_alert.setVisibility(View.INVISIBLE);
+                else panel_alert.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     private void prepFrequency() {
@@ -40,5 +60,9 @@ public class NewHabit extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
 
         sp_frequency.setAdapter(adapter);
+    }
+
+    public void exitButtonClicked(View view) {
+        finish();
     }
 }
