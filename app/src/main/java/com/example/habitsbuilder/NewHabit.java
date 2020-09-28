ee14831ede6c499df1cb478ec9b1978ae4edf1c2
quanphanhong <18520239@gm.uit.edu.vn>
2020-09-28 15:22:36 +0700

    add 'alert time picker' dialog

--- app/src/main/java/com/example/habitsbuilder/NewHabit.java
+++ app/src/main/java/com/example/habitsbuilder/NewHabit.java
@@ -1,24 +1,39 @@
 package com.example.habitsbuilder;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.database.DataSetObserver;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class NewHabit extends AppCompatActivity {
 
     Spinner sp_frequency;
-
     LinearLayout panel_alert;
     Switch sw_alert;
+    TextView tv_alertTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +46,22 @@ public class NewHabit extends AppCompatActivity {
 
     private void init() {
         sp_frequency = (Spinner) findViewById(R.id.habit_frequency_spinner);
-
-        panel_alert = (LinearLayout) findViewById(R.id.habit_reminding_time_setting);
+        panel_alert = (LinearLayout) findViewById(R.id.habit_alert_time_layout);
+        tv_alertTime = (TextView) findViewById(R.id.alert_time_text_view);
         sw_alert = (Switch) findViewById(R.id.habit_alert_switch);
         panel_alert.setVisibility(View.INVISIBLE);
         sw_alertChangedListener();
-
     }
 
     private void sw_alertChangedListener() {
         sw_alert.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (!isChecked) panel_alert.setVisibility(View.INVISIBLE);
-                else panel_alert.setVisibility(View.VISIBLE);
+                if (!isChecked) {
+                    panel_alert.setVisibility(View.INVISIBLE);
+                } else {
+                    panel_alert.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
@@ -62,7 +79,49 @@ public class NewHabit extends AppCompatActivity {
         sp_frequency.setAdapter(adapter);
     }
 
-    public void exitButtonClicked(View view) {
-        finish();
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void timePickerClicked(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.time_picker, null);
+
+        final TimePicker timePicker = alertLayout.findViewById(R.id.time_picker);
+        timePicker.setIs24HourView(true);
+        timePicker.setHour(Integer.parseInt(tv_alertTime.getText().subSequence(0, 2).toString()));
+        timePicker.setMinute(Integer.parseInt(tv_alertTime.getText().subSequence(3, 5).toString()));
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Choose Reminding Time");
+        alert.setView(alertLayout);
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "You've just canceled the process!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String currentTime;
+
+                if (timePicker.getHour() < 10)
+                    currentTime = "0" + timePicker.getHour() + ":";
+                else
+                    currentTime = timePicker.getHour() + ":";
+
+                if (timePicker.getMinute() < 10)
+                    currentTime += "0" + timePicker.getMinute();
+                else
+                    currentTime += timePicker.getMinute();
+
+                tv_alertTime.setText(currentTime);
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
+
+    public void exitButtonClicked(View view) { finish(); }
 }
